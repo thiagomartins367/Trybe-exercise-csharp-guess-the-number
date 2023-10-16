71457cda75c29824f18725e80555b1c8c6459fcd
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-10-16 15:54:24 -0300

    feat(GuessingGame.cs): Verifique a resposta da jogada

--- src/guessing-number/GuessingGame.cs
+++ src/guessing-number/GuessingGame.cs
@@ -67,7 +67,18 @@ public class GuessNumber
     //4 - Verifique a resposta da jogada
     public string AnalyzePlay()
     {
-        throw new NotImplementedException();
+        if (this.userValue < this.randomValue)
+        {
+            return "Tente um número MAIOR";
+        }
+        else if (this.userValue > this.randomValue)
+        {
+            return "Tente um número MENOR";
+        }
+        else
+        {
+            return "ACERTOU!";
+        }
     }
 
     //7 - Adicione uma opção para reiniciar o jogo
